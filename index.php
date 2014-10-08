ebf8e2ec49e5209cd21cef9e6f39489b9277683e
Tyler Ward <twardnw@gmail.com>
2014-10-08 11:42:35 -0700

    space/tab cleanup

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <html>
-	<head>	
-		<!-- regular CSS for page -->	
+	<head>
+		<!-- regular CSS for page -->
 		<link rel="stylesheet" type="text/css" href="style.css">
-		
+
 		<?php
-			// set up variables and arrays 
+			// set up variables and arrays
 			$beername = array();
 			$style = array();
-			$whobrew = array();					
-			$whobrew = array();					
-			$abv = array();					
+			$whobrew = array();
+			$whobrew = array();
+			$abv = array();
 			$hops = array();
 			$srm = array();
 			$status = array();
 			$beernamel = array();
 
-			
+
 			// set URL/Paths for csv and recipe pages
 			$beersurl = "beers.csv";
 			$tap1fB = "";
@@ -24,7 +24,7 @@
 			$tap4fB = "";
 			$tap5fB = "";
 			$tap6fB = "";
-			
+
 			// writes csv info into arrays
 			$handle = fopen($beersurl, "r");
 				while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
@@ -40,7 +40,7 @@
 			fclose($handle);
 		?>
 	</head>
-	
+
 	<body>
 		<div id="header">
 			<div id="headerboxa">
@@ -52,36 +52,36 @@
 			</div>
 			<div id="headerboxa">
 				<img src="/kegerface/images/blank.png" height="100">
-			</div>		
+			</div>
 			<div id="headerboxb">
 				<h1>BEERS ON TAP</h1>
 			</div>
-		</div>	
+		</div>
 		<br />
 		<!-- Background div for column headers -->
 		<div id="TitleBarBack">
 		</div>
-		
+
 		<div id="TitleBarBeerPic">
 		</div>
-		
+
 		<div id="TitleBarBeerName">
 		NAME
 		</div>
-		
+
 		<div id="TitleBarInfo">
 		INFO
 		</div>
-		
+
 		<div id="TitleBarKeg">
 		STATUS
 		</div>
-		
+
 		<!-- Tap 1 -->
 		<div id="content-row1a">
 			<div id="BeerPic">
 				<img src="/kegerface/images/SRM <?php echo $srm['Beer1']; ?>.png" height="150">
-			</div>	
+			</div>
 			<div id="<?php echo $beernamel['Beer1']; ?>">
 				<?php echo "<h1>", $beername['Beer1'], "</h1>";
 					echo "<h2>", $style['Beer1'], "<br />", $whobrew['Beer1'], "</h2>"; ?>
@@ -95,15 +95,15 @@
 				<img src="/kegerface/images/kegs/<?php echo $status['Beer1']; ?>.png" width="80">
 			</div>
 		</div>
-		
+
 		<!-- Tap 2 -->
 		<div id="content-row2">
 			<div id="content-row2g">
-			</div>		
+			</div>
 			<div id="content-row2a">
 			<div id="BeerPic">
 				<img src="/kegerface/images/SRM <?php echo $srm['Beer2']; ?>.png" height="150">
-			</div>	
+			</div>
 			<div id="<?php echo $beernamel['Beer2']; ?>">
 				<?php echo "<h1>", $beername['Beer2'], "</h1>";
 					echo "<h2>", $style['Beer2'], "<br />", $whobrew['Beer2'], "</h2>"; ?>
@@ -118,12 +118,12 @@
 				</div>
 			</div>
 		</div>
-		
-		<!-- Tap 3 -->		
+
+		<!-- Tap 3 -->
 		<div id="content-rowe">
 			<div id="BeerPic">
 				<img src="/kegerface/images/SRM <?php echo $srm['Beer3']; ?>.png" height="150">
-			</div>	
+			</div>
 			<div id="<?php echo $beernamel['Beer3']; ?>">
 				<?php echo "<h1>", $beername['Beer3'], "</h1>";
 					echo "<h2>", $style['Beer3'], "<br />", $whobrew['Beer3'], "</h2>"; ?>
@@ -141,11 +141,11 @@
 		<!-- Tap 4 -->
 		<div id="content-row2">
 			<div id="content-row2g">
-			</div>		
+			</div>
 			<div id="content-row2a">
 				<div id="BeerPic">
 				<img src="/kegerface/images/SRM <?php echo $srm['Beer4']; ?>.png" height="150">
-			</div>	
+			</div>
 			<div id="<?php echo $beernamel['Beer4']; ?>">
 				<?php echo "<h1>", $beername['Beer4'], "</h1>";
 					echo "<h2>", $style['Beer4'], "<br />", $whobrew['Beer4'], "</h2>"; ?>
@@ -165,7 +165,7 @@
 		<div id="content-rowe">
 			<div id="BeerPic">
 				<img src="/kegerface/images/SRM <?php echo $srm['Beer5']; ?>.png" height="150">
-			</div>	
+			</div>
 			<div id="<?php echo $beernamel['Beer5']; ?>">
 				<?php echo "<h1>", $beername['Beer5'], "</h1>";
 					echo "<h2>", $style['Beer5'], "<br />", $whobrew['Beer5'], "</h2>"; ?>
@@ -183,11 +183,11 @@
 		<!-- Tap 6 -->
 		<div id="content-row2">
 			<div id="content-row2g">
-			</div>		
+			</div>
 			<div id="content-row2a">
 				<div id="BeerPic">
 				<img src="/kegerface/images/SRM <?php echo $srm['Beer6']; ?>.png" height="150">
-			</div>	
+			</div>
 			<div id="<?php echo $beernamel['Beer6']; ?>">
 				<?php echo "<h1>", $beername['Beer6'], "</h1>";
 					echo "<h2>", $style['Beer6'], "<br />", $whobrew['Beer6'], "</h2>"; ?>
@@ -201,15 +201,15 @@
 					<img src="/kegerface/images/kegs/<?php echo $status['Beer6']; ?>.png" width="80">
 				</div>
 			</div>
-		</div>		
+		</div>
 
 		<div id="footer">
 			Kegerface 2.13.13v
 		</div>
-		
+
 		<!-- this overlays a graphic that gives a little more grunge look to everything -->
 		<span></span>
-		
+
 		<!-- overlay divs for links to recipes -->
 		<a target="_blank" href="<?php echo $tap1fB; ?>"><div id="links1"></div></a>
 		<a target="_blank" href="<?php echo $tap2fB; ?>"><div id="links2"></div></a>
